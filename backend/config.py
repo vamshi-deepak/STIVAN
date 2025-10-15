8c154b8a0f2c28f035e1498bf94572cbb93466cc
vamshi deepak <vamshideepak94@gmail.com>
2025-10-15 10:24:39 +0530

    Clean Repo

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
-    # MongoDB Atlas URI
-    MONGO_URI = "mongodb+srv://parthivvanapalli:<your-password-here>@stivan.mjhcfxg.mongodb.net/stivan?retryWrites=true&w=majority"
-
-# Export the MONGO_URI at module level
-MONGO_URI = Config.MONGO_URI
