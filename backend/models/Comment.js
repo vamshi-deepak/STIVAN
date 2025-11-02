e5d6750d2f5a9c69f32a0e9b6a970fa457ebd5cd
vamshi deepak <vamshideepak94@gmail.com>
2025-11-02 11:40:08 +0530

    Deleting old files

--- backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true, trim: true, maxlength: 2000 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', commentSchema);
